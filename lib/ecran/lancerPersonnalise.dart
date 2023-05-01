054df93ec963025b2c82347c6e7a05b8e0ca1eb3
mathieu <mathieuvanderbregt@gmail.com>
2023-05-01 20:29:51 +0200

    test push

--- lib/ecran/lancerPersonnalise.dart
+++ lib/ecran/lancerPersonnalise.dart
@@ -29,7 +29,7 @@ class _LancerDesPersonnaliseState extends State<LancerDesPersonnalise> {
             ),
             child: const Image(image: AssetImage("assets/paradice_logo.png")),
           ),
-          
+          Text("est"),
         ],
       ),
     );
